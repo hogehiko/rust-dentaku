50db995acfe148535f835f0177d29c4708c76b11
hogehiko <takrockjp@gmail.com>
2018-07-03 20:32:01 +0900

    Impl add/sub/mul/div

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,96 @@ extern crate pest;
 extern crate pest_derive;
 
 use pest::Parser;
+use pest::iterators::*;
+
+use std::io;
 
 #[derive(Parser)]
 #[grammar = "ident.pest"]
 struct IdentParser;
 
 fn main() {
-    let pairs = IdentParser::parse(Rule::expression, "123+4*5+1*2*3").unwrap_or_else(|e| panic!("{}", e));
-    println!("{}", pairs);
+    let mut input = String::new();
 
-    for pair in pairs {
-
-        for inner_pair in pair.into_inner() {
-            let inner_span = inner_pair.clone().into_span();
-            match inner_pair.as_rule() {
-                Rule::number => println!("Number:   {}", inner_span.as_str()),
-                Rule::digit_non_zero => println!("Digit:   {}", inner_span.as_str()),
-                Rule::digit => println!("Digit-nz:   {}", inner_span.as_str()),
-                Rule::addsub_exp => println!("AddSub:   {}", inner_span.as_str()),
-                Rule::expression => println!("Expression:   {}", inner_span.as_str()),
-                _ => {
-                    println!("Unreachable:   {}", inner_span.as_str());
-                    unreachable!()
+    loop{
+        match io::stdin().read_line(&mut input){
+            Ok(bytes_read) => {
+                let pairs = IdentParser::parse(Rule::expression, &input).unwrap_or_else(|e| panic!("{}", e));
+                println!("{}", pairs);
+                for pair in pairs {
+                    println!("result {}", calc(&pair));
                 }
-            };
+
+            } 
+
+            Err(err) => {
+                println!("error: {}", err);
+                break;
+            }
         }
+    }
+}
+
+fn  calc <'a>(p:&Pair<'a, Rule>) -> i64{
+    
+    match p.as_rule() {
+        Rule::number => {
+            p.as_str().parse::<i64>().unwrap()
+        }
+        Rule::addsub_exp => {
+            let mut current_value = 0;
+            //1 add 2 sub
+            let mut state = 0;
+
+            for i in p.clone().into_inner(){
+
+                match i.as_rule() {
+                    Rule::plus => state = 1,
+                    Rule::sub => state=2,
+                    _ => {
+                        let right = calc(&i);
+                        match state {
+                            0 => current_value = right,
+                            1 => current_value += right,
+                            2 => current_value -= right,
+                            _ => panic!("Undefined")
+                        }
+                    }
+                }
+            }
+            // }
+            current_value
+        }
+        Rule::muldiv_exp => {
+            let mut current_value = 0;
+            //1 add 2 sub
+            let mut state = 0;
+
+            for i in p.clone().into_inner(){
+
+                match i.as_rule() {
+                    Rule::mul => state = 1,
+                    Rule::div => state = 2,
+                    _ => {
+                        let right = calc(&i);
+                        match state {
+                            0 => current_value = right,
+                            1 => current_value *= right,
+                            2 => current_value /= right,
+                            _ => panic!("Undefined")
+                        }
+                    }
+                }
+            }
+            current_value
+        }
+        Rule::expression=>{
+            for i in p.clone().into_inner(){
+                return calc(&i)
+            }
+            0
+        }
+        x => panic!("Undefined behavior {}",p.as_str())
+
     }
 }
